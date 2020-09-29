c333d0387ac3d5551b939bb944fcad06ff19ad3a
Quentin Tavernari <ayroxx@hotmail.fr>
2020-09-29 16:57:37 +0200

    Added welcome text on homepage with console effect CSS

---
+++ src/components/CssEffects/ConsoleEffect/index.js
@@ -0,0 +1,68 @@
+// == Import npm
+import React, { useEffect } from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const ConsoleEffect = () => {
+  useEffect(() => {
+    consoleText(['Bonjour !', 'Bienvenue sur mon site.', "Accédez aux catégories à l'aide du header"], 'text', ['tomato', 'rebeccapurple', 'lightblue']);
+  });
+  // function([string1, string2],target id,[color1,color2])
+
+  function consoleText(words, id, colors) {
+    if (colors === undefined) colors = ['#fff'];
+    let visible = true;
+    const con = document.getElementById('console');
+    let letterCount = 1;
+    let x = 1;
+    let waiting = false;
+    const target = document.getElementById(id);
+    target.setAttribute('style', 'color:' + colors[0]);
+    window.setInterval(() => {
+      if (letterCount === 0 && waiting === false) {
+        waiting = true;
+        target.innerHTML = words[0].substring(0, letterCount);
+        window.setTimeout(() => {
+          const usedColor = colors.shift();
+          colors.push(usedColor);
+          const usedWord = words.shift();
+          words.push(usedWord);
+          x = 1;
+          target.setAttribute('style', 'color:' + colors[0]);
+          letterCount += x;
+          waiting = false;
+        }, 1000);
+      } else if (letterCount === words[0].length + 1 && waiting === false) {
+        waiting = true;
+        window.setTimeout(() => {
+          x = -1;
+          letterCount += x;
+          waiting = false;
+        }, 1000);
+      } else if (waiting === false) {
+        target.innerHTML = words[0].substring(0, letterCount);
+        letterCount += x;
+      }
+    }, 120);
+    window.setInterval(() => {
+      if (visible === true) {
+        con.className = 'console-underscore hidden';
+        visible = false;
+      } else {
+        con.className = 'console-underscore';
+        visible = true;
+      }
+    }, 400);
+  }
+  return (
+    <div className="console-container">
+      <span id="text" />
+      <div className="console-underscore" id="console">&#95;</div>
+    </div>
+  );
+};
+
+// == Export
+export default ConsoleEffect;
--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 // == Import
 import Header from '../Header';
+import ConsoleEffect from '../CssEffects/ConsoleEffect';
 import './styles.css';
 
 // == Composant
 const Homepage = () => (
   <div className="app">
     <Header />
+    <ConsoleEffect />
   </div>
 );
 
