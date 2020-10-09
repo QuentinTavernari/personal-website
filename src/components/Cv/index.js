cb4d3bc5face7b798a5ea83e7a3f9e3a7a5ac64e
Quentin Tavernari <ayroxx@hotmail.fr>
2020-10-09 14:22:43 +0200

    CV page : added some CSS and accordion on categories

--- src/components/Cv/index.js
+++ src/components/Cv/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Fade from 'react-reveal/Fade';
 // import Swing from 'react-reveal/Swing';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 
 // == Import
 import Header from '../Header';
@@ -37,80 +39,125 @@ const Cv = () => (
         </div>
       </div>
       <div className="cv-body">
-        <Fade right cascade>
-          <h3 className="cv-body-experiences">Experiences</h3>
-          <div className="cv-body-experiences-item">
-            <p className="cv-body-experiences-job">Chef de marché marketplace</p>
-            <p className="cv-body-experiences-company">Groupe Fnac-Darty | Avr 2016 - Juin 2019</p>
-            <ul>
-              <li className="cv-body-experiences-description">- Gestion des vendeurs clés sur les produits informatiques (CA du périmètre de ce poste supérieur à 10M€ annuels)</li>
-              <li className="cv-body-experiences-description">- Développement du CA du périmètre via différents leviers (offres en ligne, animation commerciale, recrutement, web-merchandising)</li>
-            </ul>
-          </div>
-          <div className="cv-body-experiences-item">
-            <p className="cv-body-experiences-job">Category manager web & magasins</p>
-            <p className="cv-body-experiences-company">Groupe Fnac-Darty | Nov 2014 - Janv 2016</p>
-            <ul>
-              <li className="cv-body-experiences-description">- Gestion de plusieurs familles de produits pour fnac.com et les 140 magasins (PC de bureau, moniteurs, objets connectés, sport)</li>
-              <li className="cv-body-experiences-description">- Création et suivi des gammes de produits</li>
-              <li className="cv-body-experiences-description">- Gestion des commandes et des stocks</li>
-              <li className="cv-body-experiences-description">- Développement du CA via l'animation commerciale</li>
-            </ul>
-          </div>
-          <div className="cv-body-experiences-item">
-            <p className="cv-body-experiences-job">Web merchandiser fnac.com</p>
-            <p className="cv-body-experiences-company">Groupe Fnac-Darty | Fev 2014 - Aout 2014</p>
-            <p className="cv-body-experiences-description">Gestion du web merchandising pour les produits informatiques sur le site Fnac.com :</p>
-            <ul>
-              <li className="cv-body-experiences-description">- Paramétrage des actions commerciales web</li>
-              <li className="cv-body-experiences-description">- Proposition et application d'actions web merchandising</li>
-              <li className="cv-body-experiences-description">- Brief et validation de newsletters</li>
-            </ul>
-          </div>
-        </Fade>
-        <Fade left cascade>
-          <h3 className="cv-body-education">Formations</h3>
-          <div className="cv-body-education-item">
-            <p className="cv-body-education-school">Ecole O'clock</p>
-            <p className="cv-body-education-training">Formation Full Stack Javascript | Mars - Dec 2020</p>
-            <p>735 heures de formation qui m'ont permises d'approfondir mes connaissances en Javascript et de devenir autonome sur React/Redux et Node.js</p>
-          </div>
-          <div className="cv-body-education-item">
-            <p className="cv-body-education-school">Université de Paris Est Creteil</p>
-            <p className="cv-body-education-training">Master 1 et 2 E-Commerce | Sept 2013 - Dec 2015</p>
-            <p>Compétences acquises en : E-commerce, gestion de projet, cybersécurité, acquisition de trafic (SEO, SEA, CRM)</p>
-          </div>
-          <div className="cv-body-education-item">
-            <p className="cv-body-education-school">Université de Bourgogne</p>
-            <p className="cv-body-education-training">Licence Pro Commercialisation de produits en ligne | Oct 2012 - Juin 2013</p>
-            <p>Compétences acquises en : Web marketing, création de sites web, graphisme, développement web (HTML5 / CSS3)</p>
-          </div>
-          <div className="cv-body-education-item">
-            <p className="cv-body-education-school">Université de Bourgogne</p>
-            <p className="cv-body-education-training">DUT Techniques de commercialisation | Sept 2010 - Juin 2012</p>
-            <p>Compétences acquises en : Marketing, négociation, droit, économie, communication</p>
-          </div>
-        </Fade>
-        <Fade right cascade>
-          <h3 className="cv-body-languages">Langues</h3>
-          <div className="cv-body-languages-item">
-            <p className="cv-body-languages-name">Anglais</p>
-            <ProgressBar className="cv-body-languages-progressbar" animated now={75} />
-          </div>
-          <div className="cv-body-languages-item">
-            <p className="cv-body-languages-name">Italien</p>
-            <ProgressBar className="cv-body-languages-progressbar" animated now={35} />
-          </div>
-        </Fade>
-        <Fade left cascade>
-          <h3 className="cv-body-hobbies">Loisirs</h3>
-          <div className="cv-body-hobbies-item">
-            <p className="cv-body-hobbies-description">J'ai déjà eu la chance de visiter une bonne dizaine de pays... mais dans 20 ans j'aimerais en avoir fait le triple !</p>
-          </div>
-          <div className="cv-body-hobbies-item">
-            <p className="cv-body-hobbies-description">J'aime l'informatique depuis tout petit. Je pourrais parler carte graphique pendant des heures...</p>
-          </div>
-        </Fade>
+        <Accordion className="accordion">
+          <Fade right cascade>
+            <Card className="accordion-card">
+              <div className="cv-body-experiences">
+                <Accordion.Toggle as={Card.Header} variant="link" eventKey="0">
+                  <div className="cv-body-experiences-title">
+                    <h3>Experiences</h3>
+                  </div>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                  <div>
+                    <div className="cv-body-experiences-item">
+                      <p className="cv-body-experiences-job">Chef de marché marketplace</p>
+                      <p className="cv-body-experiences-company">Groupe Fnac-Darty | Avr 2016 - Juin 2019</p>
+                      <ul>
+                        <li className="cv-body-experiences-description">- Gestion des vendeurs clés sur les produits informatiques (CA du périmètre de ce poste supérieur à 10M€ annuels)</li>
+                        <li className="cv-body-experiences-description">- Développement du CA du périmètre via différents leviers (offres en ligne, animation commerciale, recrutement, web-merchandising)</li>
+                      </ul>
+                    </div>
+                    <div className="cv-body-experiences-item">
+                      <p className="cv-body-experiences-job">Category manager web & magasins</p>
+                      <p className="cv-body-experiences-company">Groupe Fnac-Darty | Nov 2014 - Janv 2016</p>
+                      <ul>
+                        <li className="cv-body-experiences-description">- Gestion de plusieurs familles de produits pour fnac.com et les 140 magasins (PC de bureau, moniteurs, objets connectés, sport)</li>
+                        <li className="cv-body-experiences-description">- Création et suivi des gammes de produits</li>
+                        <li className="cv-body-experiences-description">- Gestion des commandes et des stocks</li>
+                        <li className="cv-body-experiences-description">- Développement du CA via l'animation commerciale</li>
+                      </ul>
+                    </div>
+                    <div className="cv-body-experiences-item">
+                      <p className="cv-body-experiences-job">Web merchandiser fnac.com</p>
+                      <p className="cv-body-experiences-company">Groupe Fnac-Darty | Fev 2014 - Aout 2014</p>
+                      <p className="cv-body-experiences-description">Gestion du web merchandising pour les produits informatiques sur le site Fnac.com :</p>
+                      <ul>
+                        <li className="cv-body-experiences-description">- Paramétrage des actions commerciales web</li>
+                        <li className="cv-body-experiences-description">- Proposition et application d'actions web merchandising</li>
+                        <li className="cv-body-experiences-description">- Brief et validation de newsletters</li>
+                      </ul>
+                    </div>
+                  </div>
+                </Accordion.Collapse>
+              </div>
+            </Card>
+          </Fade>
+          <Fade left cascade>
+            <Card className="accordion-card">
+              <div className="cv-body-education">
+                <Accordion.Toggle as={Card.Header} variant="link" eventKey="1">
+                  <div className="cv-body-education-title"><h3>Formations</h3></div>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="1">
+                  <div>
+                    <div className="cv-body-education-item">
+                      <p className="cv-body-education-school">Ecole O'clock</p>
+                      <p className="cv-body-education-training">Formation Full Stack Javascript | Mars - Dec 2020</p>
+                      <p>735 heures de formation qui m'ont permises d'approfondir mes connaissances en Javascript et de devenir autonome sur React/Redux et Node.js</p>
+                    </div>
+                    <div className="cv-body-education-item">
+                      <p className="cv-body-education-school">Université de Paris Est Creteil</p>
+                      <p className="cv-body-education-training">Master 1 et 2 E-Commerce | Sept 2013 - Dec 2015</p>
+                      <p>Compétences acquises en : E-commerce, gestion de projet, cybersécurité, acquisition de trafic (SEO, SEA, CRM)</p>
+                    </div>
+                    <div className="cv-body-education-item">
+                      <p className="cv-body-education-school">Université de Bourgogne</p>
+                      <p className="cv-body-education-training">Licence Pro Commercialisation de produits en ligne | Oct 2012 - Juin 2013</p>
+                      <p>Compétences acquises en : Web marketing, création de sites web, graphisme, développement web (HTML5 / CSS3)</p>
+                    </div>
+                    <div className="cv-body-education-item">
+                      <p className="cv-body-education-school">Université de Bourgogne</p>
+                      <p className="cv-body-education-training">DUT Techniques de commercialisation | Sept 2010 - Juin 2012</p>
+                      <p>Compétences acquises en : Marketing, négociation, droit, économie, communication</p>
+                    </div>
+                  </div>
+                </Accordion.Collapse>
+              </div>
+
+            </Card>
+          </Fade>
+          <Fade right cascade>
+            <Card className="accordion-card">
+              <div className="cv-body-languages">
+                <Accordion.Toggle as={Card.Header} variant="link" eventKey="2">
+                  <div className="cv-body-languages-title"><h3>Langues</h3></div>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="2">
+                  <div>
+                    <div className="cv-body-languages-item">
+                      <p className="cv-body-languages-name">Anglais</p>
+                      <ProgressBar className="cv-body-languages-progressbar" animated now={75} />
+                    </div>
+                    <div className="cv-body-languages-item">
+                      <p className="cv-body-languages-name">Italien</p>
+                      <ProgressBar className="cv-body-languages-progressbar" animated now={35} />
+                    </div>
+                  </div>
+                </Accordion.Collapse>
+              </div>
+            </Card>
+          </Fade>
+          <Fade left cascade>
+            <Card className="accordion-card">
+              <div className="cv-body-hobbies">
+                <Accordion.Toggle as={Card.Header} variant="link" eventKey="3">
+                  <div className="cv-body-hobbies-title"><h3>Loisirs</h3></div>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="3">
+                  <div>
+                    <div className="cv-body-hobbies-item">
+                      <p className="cv-body-hobbies-description">J'ai déjà eu la chance de visiter une bonne dizaine de pays... mais dans 20 ans j'aimerais en avoir fait le triple !</p>
+                    </div>
+                    <div className="cv-body-hobbies-item">
+                      <p className="cv-body-hobbies-description">J'aime l'informatique depuis tout petit. Je pourrais parler carte graphique pendant des heures...</p>
+                    </div>
+                  </div>
+                </Accordion.Collapse>
+              </div>
+            </Card>
+          </Fade>
+        </Accordion>
       </div>
     </div>
     <Footer />
--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,12 +1,10 @@
 // == Import npm
 import React from 'react';
-import Zoom from 'react-reveal/Zoom';
-import Fade from 'react-reveal/Fade';
-import Tada from 'react-reveal/Tada';
+import Pulse from 'react-reveal/Pulse';
+import Jump from 'react-reveal/Jump';
 
 // == Import
 import Header from '../Header';
-import ConsoleEffect from '../CssEffects/ConsoleEffect';
 import Footer from '../Footer';
 import './styles.scss';
 
@@ -16,12 +14,12 @@ const Homepage = () => (
     <Header />
     <div className="homepage">
       <div className="homepage-introduction">
-        <Tada><h2 className="homepage-title">Bienvenue !</h2></Tada>
-        <Fade left><p className="homepage-content">Je m'appelle Quentin, j'ai 28 ans et je suis à la recherche d'un emploi en tant que développeur front-end.</p></Fade>
-        <Zoom><p className="homepage-content">J'ai réalisé ce site pour mettre à disposition un aperçu de mon cv et de mes compétences, et pour y entreposer quelques tests persos.</p></Zoom>
-        <Fade right><p className="homepage-content"> Si vous êtes intéressé(e) par mon profil, vous pouvez m'envoyer un mail via la rubrique "me contacter".</p></Fade>
+        <Jump><h2 className="homepage-title">Bienvenue !</h2></Jump>
+        <Pulse left><p className="homepage-content">Je m'appelle Quentin, j'ai 28 ans et je suis à la recherche d'un emploi en tant que développeur front-end.</p></Pulse>
+        <Pulse><p className="homepage-content">J'ai réalisé ce site pour mettre à disposition un aperçu de mon cv et de mes compétences, et pour y entreposer quelques tests persos.</p></Pulse>
+        <Pulse right><p className="homepage-content"> Si vous êtes intéressé(e) par mon profil, vous pouvez m'envoyer un mail via la rubrique "me contacter".</p></Pulse>
+        <Jump><span>Bonne visite</span></Jump>
       </div>
-      <ConsoleEffect />
     </div>
     <Footer />
   </div>
